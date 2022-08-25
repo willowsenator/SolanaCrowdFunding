d85cf5e891bcf11c41054b95458f9a5efe3fbd50
willowsenator <willowsenator@gmail.com>
2022-08-25 22:52:33 +0200

    CheckIfSolflareWallet  is installed and connect to it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [walletAddress, setWalletAddress] = useState(null);
+    const solflare = window.solflare;
+    const isSolflareInstalled = solflare && solflare.isSolflare;
+    const checkIfWalletIsConnect = () => {
+        try {
+
+            if (isSolflareInstalled) {
+                console.log("Solflare Wallet installed");
+                solflare.connect({onlyIfTrusted: true});
+                solflare.on('connect', () => {
+                    console.log('Solflare Wallet Connected with publicKey: ', solflare.publicKey.toString());
+                    setWalletAddress(solflare.publicKey.toString());
+                });
+            } else {
+                console.log("Install a Solana Wallet");
+
+            }
+
+
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+    const connectWallet = () => {
+        if (isSolflareInstalled) {
+            solflare.connect();
+            solflare.on('connect', () => {
+                console.log('Solflare Wallet Connected with publicKey: ', solflare.publicKey.toString());
+                setWalletAddress(solflare.publicKey.toString());
+            });
+        }
+    }
+
+    const renderNotConnectedContainer = () => (
+        <button onClick={connectWallet}>Connect to Wallet</button>
+    )
+
+    useEffect(() => {
+        const onLoad = async () => {
+            await checkIfWalletIsConnect();
+        }
+        window.addEventListener("load", onLoad);
+        return () => window.removeEventListener("load", onLoad);
+    },);
+
+    return <div className="App">{!walletAddress && renderNotConnectedContainer()}</div>
 }
 
 export default App;
